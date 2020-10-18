1595a4739526e6c7304d5399b599f4fc2b8bed33
licler <ute3@yandex.ru>
2020-10-18 16:43:40 +0300

    start

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    $('.quotes-slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/previous.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.png" alt=""></button>',
+        autoplay: true
+
+    });
+
+    $('.epigraph-slider').slick({
+
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/previous.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.png" alt=""></button>',
+        autoplay: true
+
+    });
+
+
+});
