112128fb18f2d109f3103ed8f3ae74b62765888f
licler <ute3@yandex.ru>
2020-10-23 21:44:46 +0300

    advantages

--- app/js/main.js
+++ app/js/main.js
@@ -14,14 +14,17 @@ $(function () {
         autoplay: true
 
     });
-
-    // $('.slider__items').slick({
-    //     autoplay: true,
-    //     arrows: false,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1
-
-    // });
-
+    $('.advantages__item-title').on('click', function() {
+     $('.advantages__header').toggleClass('.advantages__header--expanded');
+     $('.advantages__item-wrapper').toggleClass('advantages__item-wrapper--active');
+    });
+    $('.creativity__item-title').on('click', function() {
+        $('.creativity__header').toggleClass('.creativity__header--expanded');
+        $('.creativity__item-wrapper').toggleClass('creativity__item-wrapper--active');
+       });
+       $('.design__item-title').on('click', function() {
+        $('.design__header').toggleClass('.design__header--expanded');
+        $('.design__item-wrapper').toggleClass('design__item-wrapper--active');
+       });
 
 });
