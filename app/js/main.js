9a403232155a02bb89fd20e10e8fe35f7b1e47d2
licler <ute3@yandex.ru>
2020-10-30 21:46:06 +0300

    adaptiv clients

--- app/js/main.js
+++ app/js/main.js
@@ -23,17 +23,19 @@ $(function () {
 
     });
     $('.advantages__item-title').on('click', function() {
-     $('.advantages__header').toggleClass('.advantages__header--expanded');
+     $('.advantages__header').toggleClass('advantages__header--expanded');
      $('.advantages__item-wrapper').toggleClass('advantages__item-wrapper--active');
     });
     $('.creativity__item-title').on('click', function() {
-     $('.creativity__header').toggleClass('.creativity__header--expanded');
+     $('.creativity__header').toggleClass('creativity__header--expanded');
      $('.creativity__item-wrapper').toggleClass('creativity__item-wrapper--active');
     });
     $('.design__item-title').on('click', function() {
-     $('.design__header').toggleClass('.design__header--expanded');
+     $('.design__header').toggleClass('design__header--expanded');
      $('.design__item-wrapper').toggleClass('design__item-wrapper--active');
     });
+
+
     $('.menu__big-btn').on('click', function(){
         $('.menu__list').slideToggle ();
     })
