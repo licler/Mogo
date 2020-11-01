835569d2638c644669f61e8ab355c37eb7168384
licler <ute3@yandex.ru>
2020-11-01 22:29:31 +0300

    adaptiv header

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,15 @@ $(function () {
     $('.quotes-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/previous.png" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.png" alt=""></button>',
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 830,
+              settings: {
+                arrows: false
+              }
+            }
+        ]
 
     });
 
